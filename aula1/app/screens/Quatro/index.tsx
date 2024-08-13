346e5427ce1dba52a1aa9c5b635b388e1e8b62c0
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-08-12 22:06:19 -0300

    Up Types

--- aula1/app/screens/Quatro/index.tsx
+++ aula1/app/screens/Quatro/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Image } from 'react-native';
 import { styles } from './styles';
 import logo from '../../assets/adaptive-icon.png';
 
